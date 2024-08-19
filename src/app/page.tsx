1cc3bd698e604a9b485e758f22547d4a75131baf
phizura <satrioapra@gmail.com>
2024-08-19 13:09:25 +0700

    first initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
-
+import AnimeList from "./components/AnimeList";
 
 export default function Home() {
   return (
     <div>
-      <h1>phianimelist</h1>
+      <h1>Paling Populer</h1>
+      <AnimeList/>
     </div>
   )
 }
